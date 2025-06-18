a1e63f2a7794fe0d83d6c4ffed627b74f13671f0
Breithner <Breithnerap@gmail.com>
2025-06-18 15:28:53 -0500

    upload change

--- static/src/components/dark_mode/dark_mode.js
+++ static/src/components/dark_mode/dark_mode.js
@@ -1,11 +1,43 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { registry } from "@web/core/registry";
+import { Component, useState } from "@odoo/owl";
+import { cookie } from "@web/core/browser/cookie";
 
-export class Dark_mode extends Component {
-    static template = "mode_dark_module.Dark_mode";
-    static components = {};
-    static props = {};
+const SCHEMES = {
+    LIGHT: 'light',
+    DARK: 'dark',
+};
 
-    setup() {}
+class DarkMode extends Component {
+
+    setup() {
+        this.state = useState({ color_scheme: SCHEMES.LIGHT });
+    }
+
+    mounted() {
+        const storedTheme = cookie.get('color_scheme');
+        this.state.color_scheme = storedTheme || SCHEMES.LIGHT;
+        cookie.set('color_scheme', this.state.color_scheme);
+        this._applyTheme();
+    }
+
+    _applyTheme() {
+        document.body.classList.toggle('night_mode', this.state.color_scheme === SCHEMES.DARK);
+    }
+
+    toggleTheme() {
+        this.state.color_scheme = this.state.color_scheme === SCHEMES.LIGHT ? SCHEMES.DARK : SCHEMES.LIGHT;
+        cookie.set('color_scheme', this.state.color_scheme);
+        this._applyTheme();
+    }
 }
+
+DarkMode.template = "mode_dark_module.SystrayItemTemplate";
+
+export const SystrayItemTemplate = {
+    Component: DarkMode,
+    isDisplayed: () => true,
+};
+
+registry.category("systray").add("DarkModeItem", SystrayItemTemplate, { sequence: 1 });
