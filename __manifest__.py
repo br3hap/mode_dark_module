d28874d9efb2f0173e64ceb196e08817329fbd1f
Breithner <Breithnerap@gmail.com>
2025-06-18 14:41:08 -0500

    upload change

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Modo Oscuro',
+    'version': '',
+    'summary': """ Módulo que permite controlar el modo oscuro - odoo 18""",
+    'author': 'Breithner Aquituari',
+    'website': '',
+    'category': '',
+    'depends': ['base', ],
+    'data': [
+        
+    ],
+    
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
